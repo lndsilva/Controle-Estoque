828b6eb6eeead11aa8f1a15c8589d607458350d2
lndsilva <lndsilva@hotmail.com>
2024-11-14 21:11:45 -0300

    barra de progresso

--- app/src/main/java/br/com/etecia/controle_estoque/LoginActivity.java
+++ app/src/main/java/br/com/etecia/controle_estoque/LoginActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.controle_estoque;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,7 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
--- app/src/main/java/br/com/etecia/controle_estoque/SplashActivity.java
+++ app/src/main/java/br/com/etecia/controle_estoque/SplashActivity.java
@@ -1,6 +1,8 @@
 package br.com.etecia.controle_estoque;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,13 @@ public class SplashActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+
+            }
+        }, 2000);
     }
 }
